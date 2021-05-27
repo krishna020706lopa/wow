e715c4bc7302add0f6aaa12242f9b17e01511bda
krishna020706lopa <76464609+krishna020706lopa@users.noreply.github.com>
2021-05-27 10:42:46 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,23 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        this.batmanImg = loadImage("bat/Bestman-01.png")
+    }
+remove(){
+    var pos = this.umbrella.position;
+    World.remove(world,this.umbrella);
+    
+    
+}
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+            image(this.image,pos.x,pos.y+70,300,300);
+    }
+}
